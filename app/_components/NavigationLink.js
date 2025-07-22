1d9b49821233da5f87375ef0a43780c0e88d3f34
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2025-07-22 14:32:04 +0300

    Fixed Go Back link behaviour

--- app/_components/NavigationLink.js
+++ app/_components/NavigationLink.js
@@ -9,41 +9,32 @@ function NavigationLink({ href, type, page, children }) {
     "flex flex-row font-bold items-center text-[13px] md:text-base gap-4 capitalize hover:underline hover:cursor-pointer transition duration-300 ease-in-out";
   const styles = {
     link: base + linkColor,
-    button: base + " text-white",
+    button:
+      base + " text-white py-4 px-10 rounded-card-corner bg-blue-grey-800",
   };
 
   const router = useRouter();
 
-  const linkContent = (
-    <>
-      <Link href={href} className={styles[type]} onClick={() => router.back()}>
-        <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M6 9L2 5l4-4"
-            stroke={
-              type === "button"
-                ? "#cfd7ff"
-                : page === "roadmap"
-                ? "#FFFF"
-                : "#4661E6"
-            }
-            strokeWidth="2"
-            fill="none"
-            fillRule="evenodd"
-          />
-        </svg>
-        <span>{children}</span>
-      </Link>
-    </>
+  return (
+    <button className={styles[type]} onClick={() => router.back()}>
+      <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M6 9L2 5l4-4"
+          stroke={
+            type === "button"
+              ? "#cfd7ff"
+              : page === "roadmap"
+              ? "#FFFF"
+              : "#4661E6"
+          }
+          strokeWidth="2"
+          fill="none"
+          fillRule="evenodd"
+        />
+      </svg>
+      <span>{children}</span>
+    </button>
   );
-
-  if (type === "button") {
-    return (
-      <button className="py-4 px-10 rounded-card-corner bg-blue-grey-800">
-        {linkContent}
-      </button>
-    );
-  } else return linkContent;
 }
 
 export default NavigationLink;
